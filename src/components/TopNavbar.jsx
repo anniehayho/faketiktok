9060462e36ee811e62ba7e02ce9f7c88445d3bcf
Thuy Trang Pham <147356022+anniehayho@users.noreply.github.com>
2024-12-13 00:41:57 +0700

    ex7

--- src/components/TopNavbar.jsx
+++ src/components/TopNavbar.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTv, faSearch } from '@fortawesome/free-solid-svg-icons';
 
-const TopNavbar = () => {
+const TopNavbar = ({ onSearch }) => {
+  const [isSearchVisible, setIsSearchVisible] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const toggleSearch = () => {
+    setIsSearchVisible(!isSearchVisible);
+  };
+
+  const handleSearch = (e) => {
+    if (e.key === 'Enter') {
+      onSearch(searchTerm);
+      setSearchTerm('');
+      setIsSearchVisible(false);
+    }
+  };
+
   return (
     <div className='top-navbar'>
       <FontAwesomeIcon icon={faTv} className='icon'/>
       <h2>Following | <span>For You</span></h2>
-      <FontAwesomeIcon icon={faSearch} className='icon'/>
+      <FontAwesomeIcon icon={faSearch} className='icon' onClick={toggleSearch} />
+      {isSearchVisible && (
+        <input 
+          type="text" 
+          value={searchTerm} 
+          onChange={(e) => setSearchTerm(e.target.value)} 
+          onKeyDown={handleSearch} 
+          placeholder="Search..."
+          className="search-input"
+        />
+      )}
     </div>
   );
 };
